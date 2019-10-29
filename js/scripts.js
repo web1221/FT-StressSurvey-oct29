8029541a99372305075e3cc1a08e4680f950e884
Student <example@example.com>
2019-10-29 10:54:12 -0700

    add scripts, change variable names

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,39 @@
 var counter = function(arr) {
+  var total = 0;
   for (var i = 0; i < arr.length; i++) {
-  total += arr[i]
+    total += arr[i]
   }
+  return total;
 }
-var mentalSymptomsArray = [];
+var mentalHealthArray = [];
 $(document).ready(function(){
   $("form#stressTest").submit(function(event){
     event.preventDefault();
-    var add = function(number){
-      var total = 0;
-      total += number;
-    }
+    // var add = function(number){
+    //   var total = 0;
+    //   total += number;
+    // }
 
     $("input:checkbox[name=mental-symptoms]:checked").each(function(){
       var mentalSymptoms = parseInt($(this).val());
-      mentalSymptomsArray.push(mentalSymptoms);
-      console.log("mentalSymptomsArray: ", mentalSymptomsArray)
-      console.log(counter(mentalSymptomsArray));
+      mentalHealthArray.push(mentalSymptoms);
+      console.log("mentalHealthArray: ", mentalHealthArray);
       // console.log(counter(mentalSymptoms));
       // console.log("typeof mentalSymptoms: ", typeof(mentalSymptoms));
       // console.log("Mental Symptons Value: ", (add(mentalSymptoms)));
       $(".mentalSymptoms").append(mentalSymptoms + "<br>");
 
     });
+    $('input:checkbox[name=physical-symptoms]:checked').each(function() {
+      var physicalSymptoms = parseInt($(this).val());
+      mentalHealthArray.push(physicalSymptoms);
+      $('.physicalSymptoms').append(physicalSymptoms + "<br>");
+    });
+
+    $('input:checkbox[name=relaxation-techniques]:checked').each(function(){
+      var relaxationTechniques = parseInt($(this).val());
+      mentalHealthArray.push(relaxationTechniques);
+      $('.relaxationTechniques').append(relaxationTechniques + "<br>");
+    })
   });
 });
